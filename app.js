f1438de21e1b881f000cd2c04070b71e4fbf323c
dani <123590342+bisuketto0@users.noreply.github.com>
2024-05-28 09:23:41 -0300

    Make "show-pass" checbox work
    Show the password when the checkbox is checked
    and hide it when it's not.

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ let passConfirm = ''
 
 document.getElementById("password").addEventListener('keyup', function() {
   pass = this.value
-  console.log(pass)
 })
 
 document.getElementById("confirm-password").addEventListener('keyup', function() {
   passConfirm = this.value
-  console.log(passConfirm)
   if (pass != passConfirm) {
     document.getElementById("confirm-password").setCustomValidity("Passwords need to match.")
   } else {
     document.getElementById("confirm-password").setCustomValidity("")
+  }
+})
+
+document.getElementById("show-pass").addEventListener("click", function() {
+  pass = document.getElementById("password")
+  passConfirm = document.getElementById("confirm-password")
+
+  if(pass.type === "password") {
+    pass.type = "text"
+    passConfirm.type = "text"
+  } else {
+    pass.type = "password"
+    passConfirm.type = "password"
   }
 })
